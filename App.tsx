1d387bf8d6c6679446049dfcc75ac832c6b1120e
kimbeff <kimbeff@gmail.com>
2025-12-27 12:59:02 +0200

    a

--- App.tsx
+++ App.tsx
@@ -19,14 +19,5 @@ export default function App() {
         </KeywordProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
-import { StatusBar } from 'expo-status-bar';
-import { KeywordSearchScreen } from './src/screens/KeywordSearchScreen';
-
-export default function App() {
-  return (
-    <>
-      <KeywordSearchScreen />
-      <StatusBar style="auto" />
-    </>
   );
 }
